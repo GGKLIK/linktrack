6c958827d75be71364ff9dddda4fef8fd5e79805
GGKLIK <Immanuelch55@gmail.com>
2025-04-27 18:53:53 +0700

    Update track.py

--- track.py
+++ track.py
@@ -15,7 +15,7 @@ class Link(db.Model):
 def index():
     return render_template('index.html')
 
-@app.route('/track.py', methods=['POST'])
+@app.route('/linktrack/track.py', methods=['POST'])
 def create_link():
     original_url = request.form['url']
     tracking_id = str(uuid.uuid4())
